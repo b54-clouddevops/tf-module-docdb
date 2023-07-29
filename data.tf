f741661a1cdbff7ebbb017882fce7e6ac51e1448
CodingManoj <mvp2612@hotmail.com>
2023-07-28 21:54:18 -0400

    AutoPush

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@
 
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-  config {
+  config = {
     bucket = "b54-tf-remote-state"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
