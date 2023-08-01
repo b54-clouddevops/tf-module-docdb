3a14dbed12fb70c4f7be021cab9bb98bec31fe7a
CodingManoj <mvp2612@hotmail.com>
2023-07-31 21:36:49 -0400

    AutoPush

--- docdb.tf
+++ docdb.tf
@@ -7,7 +7,6 @@ resource "aws_docdb_cluster" "docdb" {
   vpc_security_group_ids   = [aws_security_group.allows_docdb.id]
   db_subnet_group_name     = aws_docdb_subnet_group.docdb_subnet_group.name
 
-
 #   backup_retention_period = 14
 #   preferred_backup_window = "02:00-05:00"
 }
