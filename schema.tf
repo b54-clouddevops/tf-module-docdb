ed8384e4a238f245e0148b0a0769fe2cc9fb1a5e
CodingManoj <mvp2612@hotmail.com>
2023-08-01 20:48:07 -0400

    AutoPush

--- schema.tf
+++ schema.tf
@@ -4,7 +4,7 @@ resource "null_resource" "docdb_schema" {
        
   depends_on = [aws_docdb_cluster.docdb , aws_docdb_cluster_instance.cluster_instances]
   provisioner "local-exec" {
-         command = <<EOF 
+         command = <<EOF
             cd /tmp 
             wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
             curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
